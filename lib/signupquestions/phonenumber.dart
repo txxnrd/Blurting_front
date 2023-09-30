8a2b93caece54f7b9dc693a7a55985ed68c1202c
txxnrd <nrbsld@korea.ac.kr>
2023-09-30 18:49:22 +0900

    set

---
+++ lib/signupquestions/phonenumber.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:blurting/signupquestions/sex.dart';  // sex.dart를 임포트
+
+class PhoneNumberPage extends StatefulWidget {
+  @override
+  _PhoneNumberPageState createState() => _PhoneNumberPageState();
+}
+
+class _PhoneNumberPageState extends State<PhoneNumberPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(''),
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings, color: Colors.black),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 25,
+            ),
+            Stack(
+              clipBehavior: Clip.none, // 이 부분 추가
+              children: [
+                // 전체 배경색 설정 (하늘색)
+                Container(
+                  height: 10,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFD9D9D9), // 하늘색
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                ),
+                // 완료된 부분 배경색 설정 (파란색)
+                Container(
+                  height: 10,
+                  width: MediaQuery.of(context).size.width * 0, // 10% 완료로 가정
+                  decoration: BoxDecoration(
+                    color: Color(0xFF303030), // 파란색
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                ),
+                Positioned(
+                  left: MediaQuery.of(context).size.width * 0 - 15,
+                  bottom: -10,
+                  child: Image.asset('assets/signupface.png', width: 30, height: 30),
+                )
+              ],
+            ),
+
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              '반가워요! 전화번호를 입력해 주세요',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700,color: Color(0xFF303030),fontFamily: 'Pretendard'),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                hintText: '010-1234-5678',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFFF66464),), // 초기 테두리 색상
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFFF66464),), // 입력할 때 테두리 색상
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color:Color(0xFFF66464),), // 선택/포커스 됐을 때 테두리 색상
+                ),
+              ),
+            ),
+
+
+            SizedBox(height: 309),
+            Container(
+              width: 350,
+              height: 48,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFFF66464),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SexPage()),
+                  );
+                },
+
+                child: Text(
+                  '인증번호 요청',
+                  style: TextStyle(
+                    fontFamily: 'Pretendard',
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FaceIconPainter extends CustomPainter {
+  final double progress;
+
+  FaceIconPainter(this.progress);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.fill;
+
+    final facePosition = Offset(size.width * progress - 10, size.height / 2);
+    canvas.drawCircle(facePosition, 5.0, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
---
+++ lib/signupquestions/sex.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+import 'package:blurting/signupquestions/phonenumber.dart';  // sex.dart를 임포트
+
+class SexPage extends StatefulWidget {
+  @override
+  _SexPageState createState() => _SexPageState();
+}
+enum Gender { male, female }
+
+class _SexPageState extends State<SexPage> {
+  Gender? _selectedGender;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(''),
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings, color: Colors.black),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 25,
+            ),
+            Stack(
+              clipBehavior: Clip.none, // 이 부분 추가
+              children: [
+                // 전체 배경색 설정 (하늘색)
+                Container(
+                  height: 10,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFD9D9D9), // 하늘색
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                ),
+                // 완료된 부분 배경색 설정 (파란색)
+                Container(
+                  height: 10,
+                  width: MediaQuery.of(context).size.width * 0.1, // 10% 완료로 가정
+                  decoration: BoxDecoration(
+                    color: Color(0xFF303030), // 파란색
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                ),
+                Positioned(
+                  left: MediaQuery.of(context).size.width * 0.1 - 15,
+                  bottom: -10,
+                  child: Image.asset('assets/signupface.png', width: 30, height: 30),
+                )
+              ],
+            ),
+
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              '당신의 성별은 무엇인가요?',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700,color: Color(0xFF303030),fontFamily: 'Pretendard'),
+            ),
+            SizedBox(height: 30),
+
+
+            SizedBox(width: 20), // 두 버튼 사이의 간격 조정
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 159, // 원하는 너비 값
+                  height: 48, // 원하는 높이 값
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      side: BorderSide(color: Color(0xFF868686), width: 2,),
+                      primary: Color(0xFF303030),
+                      backgroundColor: _selectedGender == Gender.male ? Color(0xFF868686) : Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),  // 원하는 모서리 둥글기 값
+                      ),
+                    ),
+
+                    onPressed: () {
+                      setState(() {
+                        _selectedGender = Gender.male;
+                      });
+                    },
+                    child: Text(
+                      '남성',
+                      style: TextStyle(
+                        color: Color(0xFF303030),
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+
+
+                SizedBox(width: 23), // 두 버튼 사이의 간격 조정
+
+                Container(
+                  width: 159, // 원하는 너비 값
+                  height: 48, // 원하는 높이 값
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      side: BorderSide(color: Color(0xFF868686), width: 2),
+                      primary: Color(0xFF303030),
+                      backgroundColor: _selectedGender == Gender.female ? Color(0xFF868686) : Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),  // 원하는 모서리 둥글기 값
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _selectedGender = Gender.female;
+                      });
+                    },
+                    child: Text(
+                      '여성',
+                      style: TextStyle(
+                        color: Color(0xFF303030),
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 309),
+            Container(
+              width: 350,
+              height: 48,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFFF66464),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PhoneNumberPage()),
+                  );
+                },
+
+                child: Text(
+                  '다음',
+                  style: TextStyle(
+                    fontFamily: 'Pretendard',
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FaceIconPainter extends CustomPainter {
+  final double progress;
+
+  FaceIconPainter(this.progress);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.fill;
+
+    final facePosition = Offset(size.width * progress - 10, size.height / 2);
+    canvas.drawCircle(facePosition, 5.0, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
