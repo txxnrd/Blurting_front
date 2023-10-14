d5ac47218e58ab7c907c505da249276d2db864d4
jw0202058 <jw0202058@korea.ac.kr>
2023-10-15 01:56:48 +0900

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:blurting/mainApp.dart';
-/*
+
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -8,22 +8,22 @@ void main() {
   ));
 }
 
-*/
-import 'package:blurting/signupquestions/phonenumber.dart';  // phonenumber.dart를 임포트
 
-void main() => runApp(MyApp());
+// import 'package:blurting/signupquestions/phonenumber.dart';  // phonenumber.dart를 임포트
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Phone Number App',
-      theme: ThemeData(
-        primaryColor: Colors.white,
-        backgroundColor: Colors.white,
-      ),
-      home: PhoneNumberPage(), // PhoneNumberPage를 홈으로 설정
-    );
-  }
-}
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Phone Number App',
+//       theme: ThemeData(
+//         primaryColor: Colors.white,
+//         backgroundColor: Colors.white,
+//       ),
+//       home: PhoneNumberPage(), // PhoneNumberPage를 홈으로 설정
+//     );
+//   }
+// }
