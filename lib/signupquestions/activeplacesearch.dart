8bc3e6f73084fc714d2b3179aa866df10d1baee7
yerak <yerak213@gmail.com>
2023-10-15 15:17:33 +0900

    activeplace search

--- lib/signupquestions/activeplacesearch.dart
+++ lib/signupquestions/activeplacesearch.dart
@@ -6,15 +6,9 @@ String searchText = '';
 
 // 리스트뷰에 표시할 내용
 List<String> items = ['안암동1가', '안암동2가', '안암동3가', '안암동4가'];
-List<String> itemContents = [
-  '안암동 1가',
-  '안암동 2가',
-  '안암동 3가',
-  '안암동 4가'
-];
+List<String> itemContents = ['안암동 1가', '안암동 2가', '안암동 3가', '안암동 4가'];
 
 class SearchPage extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return _SearchPage();
@@ -35,9 +29,10 @@ class _SearchPage extends State<SearchPage> {
     return MaterialApp(
       title: 'MemoApp', // 앱의 아이콘 이름
       home: Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
           toolbarHeight: 80,
-          backgroundColor: Colors.transparent, // 배경색을 투명하게 설정합니다.
+          backgroundColor: Colors.white, // 배경색을 투명하게 설정합니다.
           elevation: 0, // 그림자 효과를 제거합니다.
           leading: IconButton(
             icon: Icon(
@@ -65,7 +60,22 @@ class _SearchPage extends State<SearchPage> {
               child: TextField(
                 decoration: InputDecoration(
                   hintText: '동명(읍,면)으로 검색 (ex. 안암동)',
-                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.all(10.0),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFF868686),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFFF66464),
+                    ), // 입력할 때 테두리 색상
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFFF66464),
+                    ),
+                  ),
                 ),
                 onChanged: (value) {
                   setState(() {
@@ -89,12 +99,23 @@ class _SearchPage extends State<SearchPage> {
                   // 검색어가 없을 경우, 모든 항목 표시
                   else {
                     return Card(
-                      elevation: 3,
+                      elevation: 0,
+                      // shadowColor: null,
+                      borderOnForeground: false,
+                      color: Colors.white,
                       shape: RoundedRectangleBorder(
                           borderRadius:
-                              BorderRadius.all(Radius.elliptical(20, 20))),
+                              BorderRadius.all(Radius.elliptical(10, 10))),
                       child: ListTile(
-                          title: Text(items[index]),
+                          title: Text(
+                            items[index],
+                            style: TextStyle(
+                              color: Color(0xFF303030),
+                              fontFamily: 'Pretendard',
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                            ),
+                          ),
                           onTap: () => {
                                 cardClickEvent(context, index),
                               }),
