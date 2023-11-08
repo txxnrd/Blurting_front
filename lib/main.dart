dcb0ca61d5b7124c699144a27191cbb8561067ed
jw0202058 <jw0202058@korea.ac.kr>
2023-11-08 21:43:36 +0900

    merge: jeongwon (tapBar)

--- lib/main.dart
+++ lib/main.dart
@@ -4,31 +4,31 @@ import 'package:provider/provider.dart';
 import 'package:blurting/Static/provider.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
-void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainApp()),
-  );
-}
-
-// import 'package:blurting/signupquestions/phonenumber.dart'; // phonenumber.dart를 임포트
-
-// void main() => runApp(MyApp());
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
+// void main() {
+//   runApp(
+//     MaterialApp(
 //       debugShowCheckedModeBanner: false,
-//       title: 'Phone Number App',
-//       theme: ThemeData(
-//         primaryColor: Colors.white,
-//         backgroundColor: Colors.white,
-//       ),
-//       home: PhoneNumberPage(), // PhoneNumberPage를 홈으로 설정
-//     );
-//   }
+//       home: MainApp()),
+//   );
 // }
+
+import 'package:blurting/signupquestions/phonenumber.dart'; // phonenumber.dart를 임포트
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Phone Number App',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        backgroundColor: Colors.white,
+      ),
+      home: PhoneNumberPage(), // PhoneNumberPage를 홈으로 설정
+    );
+  }
+}
