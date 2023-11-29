654875839512a5e3522d9b4c791bc26448a4ed5e
txxnrd <nrbsld@korea.ac.kr>
2023-11-29 14:52:35 +0900

    commit

--- lib/signupquestions/phonenumber.dart
+++ lib/signupquestions/phonenumber.dart
@@ -1,16 +1,12 @@
 import 'dart:convert';
 import 'dart:async';
-import 'package:blurting/signupquestions/phonecertification.dart';
 import 'package:blurting/signupquestions/sex.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:blurting/signupquestions/token.dart'; // sex.dart를 임포트
 import 'package:blurting/config/app_config.dart';
 import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:blurting/colors/colors.dart';
 import 'package:contacts_service/contacts_service.dart';
 import 'package:permission_handler/permission_handler.dart';
